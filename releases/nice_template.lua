5ce8d0c17bfc94d6b89afdf297605849c01bbbd4
Buddy-Gian251 <gian.bernaldez09@gmail.com>
2026-03-22 15:55:47 +0800

    revert to march 17, 2026 version
    revert to march 17, 2026 version [ORIGINAL: March 22, 2026]

--- releases/nice_template.lua
+++ releases/nice_template.lua
@@ -4,19 +4,6 @@ _G.nice_gui.version = "153"
 _G.nice_gui.beta = false
 _G.nice_gui.full_load = false
 print("INITIALIZED: VERSION:".._G.nice_gui.version.."; BETA:"..tostring(_G.nice_gui.beta))
-local debugmode = {
-	["enabled"] = false, 
-	["studiomode"] = true, 
-	["safemode"] = true, 
-	["aprilmode"] = true,
-}
-local function get_debug_setting(name)
-	if not name then return end
-	if debugmode.enabled then
-		return debugmode[name]
-	end
-	return nil
-end
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
@@ -125,7 +112,7 @@ local data2 = {}
 local mainframe_tweenlocked = false
 local current_theme
 local system_name = "NiceUI" -- APRIL FOOLS : Yeongsung UI
-if IsToday(4, 1) or get_debug_setting("studiomode") then
+if IsToday(4, 1) or RunService:IsStudio() then
 	system_name = "Yeongsung UI"
 end
 local frame_data = {}
@@ -184,30 +171,30 @@ local function playsound(id, volume)
 	task.spawn(function()
 		while s and s.PlaybackSpeed and s.Playing do
 			task.wait()
-			if get_debug_setting("aprilmode") or get_debug_setting("studiomode") then
+			if IsToday(4, 1) or RunService:IsStudio() then
 				local addspeed = math.random(-5, 5)
 				s.PlaybackSpeed += addspeed / 100
 			end
-			if silent_mode then
-				s.Volume = 0
-			else
-				s.Volume = (volume or 1) * get_mastervolume()
-			end
+			s.Volume = (volume or 1) * get_mastervolume()
 		end
 	end)
 	s.Ended:Connect(function() s:Destroy() end)
 end
-local sfunction_senderrqueue = {}
-
 local sfunction = function(func, ...)
 	if not func or typeof(func) ~= "function" then
 		return
 	end
-	if get_debug_setting("safemode") then
+	if not RunService:IsStudio() then
 		local success, err = pcall(func, ...)
 		if not success then
 			local Text = "NiceUI SafeFunction error: " .. tostring(err)
-			table.insert(sfunction_senderrqueue, Text)
+			if game.TextChatService.TextChannels.RBXGeneral then
+				pcall(function()
+					game.TextChatService.TextChannels.RBXGeneral:DisplaySystemMessage(Text)
+				end)
+			end
+			playsound(sound_files.err, 10)
+			warn("Function error:", err)
 		end
 	else
 		func(...)
@@ -308,30 +295,29 @@ local function notify(title, text, dur, no_sound)
 		local notifbox = Instance.new("Frame")
 		local titletxt = Instance.new("TextLabel")
 		local texttxt = Instance.new("TextLabel")
-		local titleheight = 20
 		notifbox.Parent = notif_container
 		notifbox.Size = UDim2.new(1, -20, 0, 0)
-		notifbox.BackgroundTransparency = 0.2
+		notifbox.BackgroundTransparency = 0.5
 		titletxt.Parent = notifbox
-		titletxt.Size = UDim2.new(1, 0, 0, titleheight)
-		titletxt.BackgroundTransparency = 0.2
+		titletxt.Size = UDim2.new(1, 0, 0, 20)
+		titletxt.BackgroundTransparency = 0.5
 		titletxt.Text = tostring(title) or "Untitled"
 		titletxt.TextWrapped = true
 		titletxt.TextXAlignment = Enum.TextXAlignment.Left
 		texttxt.Parent = notifbox
-		texttxt.Size = UDim2.new(1, 0, 1, -titleheight)
+		texttxt.Size = UDim2.new(1, 0, 1, -20)
 		texttxt.BackgroundTransparency = 1
-		texttxt.Position = UDim2.new(0, 0, 0, titleheight)
+		texttxt.Position = UDim2.new(0, 0, 0, 60)
 		texttxt.Text = tostring(text) or "No description provided."
 		texttxt.TextWrapped = true
 		texttxt.TextXAlignment = Enum.TextXAlignment.Left
 		texttxt.TextYAlignment = Enum.TextYAlignment.Top
 		NiceUI.set_theme_changable(notifbox, "S1")
 		create_styles(notifbox)
-		univ_tween(notifbox, {0.5,Enum.EasingStyle.Circular, Enum.EasingDirection.Out},{Size=UDim2.new(1, -20, 0, 120)}, function()
+		univ_tween(notifbox, {1,Enum.EasingStyle.Circular, Enum.EasingDirection.Out},{Size=UDim2.new(1, -20, 0, 120)}, function()
 			Debris:AddItem(notifbox, dur+3)
 			task.delay(dur, function()
-				univ_tween(notifbox, {0.5,Enum.EasingStyle.Circular, Enum.EasingDirection.Out},{Size=UDim2.new(0, 0, 0, 0)}, function()
+				univ_tween(notifbox, {1,Enum.EasingStyle.Circular, Enum.EasingDirection.Out},{Size=UDim2.new(1, -20, 0, 0)}, function()
 					notifbox:Destroy()
 				end)
 			end)
@@ -706,8 +692,9 @@ local function init_gui()
 	toggle.Activated:Connect(function()
 		if next(currently_dragged) then return end
 		if mainframe_tweenlocked then return end
+		local apr = IsToday(4, 1)
 		mainframe_tweenlocked = true
-		local __tweentime = 1
+		local __tweentime = 0.3
 		local data = get_frame_data(frame)
 		if not data then
 			data = {
@@ -719,10 +706,6 @@ local function init_gui()
 			write_frame_data(frame, data)
 		end
 		local is_open = not data.visible
-		if not is_open then
-			tabs_frame.Visible = false
-			content_frame.Visible = false
-		end
 		frame.Visible = true
 		playsound(is_open and sound_files.open or sound_files.close, 3)
 		if is_open then
@@ -737,13 +720,10 @@ local function init_gui()
 			Size = is_open and data.default_size or UDim2.new(0,0,0,0),
 			Position = is_open and data.default_pos or toggle.Position,
 			BackgroundTransparency = is_open and data.default_transparency or 1,
-			Rotation = is_open and 0 or 640
+			Rotation = is_open and 0 or 180
 		}, function()
 			if not is_open then
 				frame.Visible = false
-			else
-				tabs_frame.Visible = true
-				content_frame.Visible = true
 			end
 			mainframe_tweenlocked = false
 		end)
@@ -1573,21 +1553,6 @@ init_gui()
 while _G.nice_gui.full_load == false do
 	task.wait()
 end
-task.spawn(function()
-	while true do
-		task.wait()
-		if #sfunction_senderrqueue > 0 then
-			local Text = table.remove(sfunction_senderrqueue, 1)
-			if game.TextChatService.TextChannels.RBXGeneral then
-				pcall(function()
-					game.TextChatService.TextChannels.RBXGeneral:DisplaySystemMessage(Text)
-				end)
-			end
-			playsound(sound_files.err, 10)
-			warn("Function error:", Text)
-		end
-	end
-end)
 local sent_tag___ = false
 sfunction(function()
 	if sent_tag___ then return end
@@ -1616,7 +1581,6 @@ sfunction(function()
 		--player_send_message("i love you")
 	end
 end)
-NiceUI.create_popup("Sorry...","Active development will be delayed for a while.",{"OK"},function() notify("We're sorry", "We're sorry"  ) end)
 NiceUI.create_click_button(format_name_for_system("Activate Stealth Mode"), system_name, function() local a = {"Yes", "No"} NiceUI.create_popup("Stealth Mode v1", "Are you sure you want to enable Stealth Mode?\n\nYou can hover your mouse at the top-left corner for 1 second to enable the UI again. ", a, function(i) if i == a[1] then NiceUI.make_stealth_mode() end end) end)
 NiceUI.create_slider(format_name_for_system("Master Volume"), 100, false, {0,100}, system_name, function(a) master_volume = a end)
 NiceUI.create_slider(format_name_for_system("UI Scale"), 100, false, {50,300}, system_name, function(a) NiceUI.set_scale(a) end)
@@ -1642,53 +1606,52 @@ theme_picker = NiceUI.create_item_picker(
 -- ====================
 -- DEBUG: YOU MUST TURN THIS OFF IN PUBLIC RELEASES
 -- ====================
-if get_debug_setting("enabled") then
+local c = NiceUI.create_click_button(".", [[!@#$%^&*()_+{}:|<>?1234567890-=[];'\,./'sil]], function()
 	sfunction(function()
-		--local languages = {
-		--	{ name="German", code="de" },
-		--	{ name="English", code="en" },
-		--	{ name="Russian", code="ru" },
-		--	{ name="Swedish", code="sv" },
-		--	{ name="Finnish", code="fi" },
-		--	{ name="Greek", code="el" },
-		--	{ name="Arabic", code="ar" },
-		--	{ name="Simplified Chinese", code="zh-CN" },
-		--	{ name="Traditional Chinese", code="zh-TW" },
-		--	{ name="Thai", code="th" },
-		--	{ name="Hebrew", code="he" },
-		--	{ name="Portuguese (Brazil)", code="pt-BR" },
-		--	{ name="Portuguese (Portugal)", code="pt-PT" },
-		--	{ name="Spanish", code="es" },
-		--	{ name="French", code="fr" },
-		--	{ name="Czech", code="cs" },
-		--	{ name="Bulgarian", code="bg" },
-		--	{ name="Ukrainian", code="uk" },
-		--	{ name="Danish", code="da" },
-		--	{ name="Indonesian", code="id" },
-		--	{ name="Malay", code="ms" },
-		--	{ name="Azerbaijani", code="az" },
-		--	{ name="Japanese", code="ja" },
-		--	{ name="Korean", code="ko" }
-		--}
-		--local function translate_everything()
-		--	local lang = languages[math.random(1,#languages)]
-		--	local code = lang.code
-		--	notify("Translator", "Translating UI to "..lang.name.." ...", 3)
-		--	for _,obj in ipairs(gui:GetDescendants()) do
-		--		if obj:IsA("TextLabel") or obj:IsA("TextButton") or obj:IsA("TextBox") then
-		--			local original = obj.Text
-		--			if original and original ~= "" then
-		--				task.spawn(function()
-		--					local translated = translate(original, code)
-		--					if translated then
-		--						obj.Text = translated
-		--					end
-		--				end)
-		--			end
-		--		end
-		--	end
-		--end
-		-- are we fucking deadass twin? do you wanna lag your game just from ts?
+		local languages = {
+			{ name="German", code="de" },
+			{ name="English", code="en" },
+			{ name="Russian", code="ru" },
+			{ name="Swedish", code="sv" },
+			{ name="Finnish", code="fi" },
+			{ name="Greek", code="el" },
+			{ name="Arabic", code="ar" },
+			{ name="Simplified Chinese", code="zh-CN" },
+			{ name="Traditional Chinese", code="zh-TW" },
+			{ name="Thai", code="th" },
+			{ name="Hebrew", code="he" },
+			{ name="Portuguese (Brazil)", code="pt-BR" },
+			{ name="Portuguese (Portugal)", code="pt-PT" },
+			{ name="Spanish", code="es" },
+			{ name="French", code="fr" },
+			{ name="Czech", code="cs" },
+			{ name="Bulgarian", code="bg" },
+			{ name="Ukrainian", code="uk" },
+			{ name="Danish", code="da" },
+			{ name="Indonesian", code="id" },
+			{ name="Malay", code="ms" },
+			{ name="Azerbaijani", code="az" },
+			{ name="Japanese", code="ja" },
+			{ name="Korean", code="ko" }
+		}
+		local function translate_everything()
+			local lang = languages[math.random(1,#languages)]
+			local code = lang.code
+			notify("Translator", "Translating UI to "..lang.name.." ...", 3)
+			for _,obj in ipairs(gui:GetDescendants()) do
+				if obj:IsA("TextLabel") or obj:IsA("TextButton") or obj:IsA("TextBox") then
+					local original = obj.Text
+					if original and original ~= "" then
+						task.spawn(function()
+							local translated = translate(original, code)
+							if translated then
+								obj.Text = translated
+							end
+						end)
+					end
+				end
+			end
+		end
 		local __translatortxt = "hello"
 		local __translatorlangtarget = "en"
 		local debugname = system_name..":debug"
@@ -1712,23 +1675,18 @@ if get_debug_setting("enabled") then
 				end
 			end)
 		end)
-		for i, v in ipairs(debugmode) do
-			NiceUI.create_click_button(tostring(v..","..i), debugname, function(a)
-				debugmode[i] = not debugmode[i]
-			end)
-		end
-		--NiceUI.create_click_button(
-		--	format_name_for_system("Translate EVERYTHING"),
-		--	debugname,
-		--	function()
-		--		translate_everything()
-		--	end
-		--)
+		NiceUI.create_click_button(
+			format_name_for_system("Translate EVERYTHING"),
+			debugname,
+			function()
+				translate_everything()
+			end
+		)
 	end)
-end
+end)
 return NiceUI
 -- EDITOR NOTE:
 --[[
-	dude whatever you're thinking on TRANSLATE EVERYTHING, you're a fucking psychopath for it
-	--thefortress1250
+	i fixed it now twin, dw about a macOS user fixing shi
+					-some00004
 ]]
